d38f6edb07461fef1a55ff4042a677bbcb181efd
hschou <hschou@student.42.us.org>
2019-07-19 00:17:06 -0700

    mod [str_is_alpha]
    remove main

--- ft_str_is_alpha.c
+++ ft_str_is_alpha.c
@@ -6,11 +6,9 @@
 /*   By: hschou <hschou@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 23:58:12 by hschou            #+#    #+#             */
-/*   Updated: 2019/07/19 00:11:53 by hschou           ###   ########.fr       */
+/*   Updated: 2019/07/19 00:16:15 by hschou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void	ft_putnbr(int nb);
 
 int		ft_str_is_alpha(char *str)
 {
@@ -29,12 +27,4 @@ int		ft_str_is_alpha(char *str)
 		i++;
 	}
 	return (alpha);
-}
-
-int		main(void)
-{
-	char str[] = "ThereIsNoSuchThingAsTree";
-
-	ft_putnbr(ft_str_is_alpha(str));
-	return (0);
 }
