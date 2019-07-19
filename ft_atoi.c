49e045078f028a00ba43bbc8766678ae425c36b6
hschou <hschou@student.42.us.org>
2019-07-18 21:16:20 -0700

    mod [atoi]
    fix bug

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,33 @@
 /*   By: hschou <hschou@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 17:08:50 by hschou            #+#    #+#             */
-/*   Updated: 2019/07/16 22:58:23 by hschou           ###   ########.fr       */
+/*   Updated: 2019/07/18 21:11:40 by hschou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_atoi(char *str)
 {
 	int i;
-	int n;
 	int sign;
+	int n;
 
 	i = 0;
-	n = 0;
 	sign = 1;
-	while (str[i] != '\0')
+	n = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-')
 	{
-		if (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
-			str[i] == '\f' || str[i] == '\r' ||
-			str[i] == ' ' || str[i] == '+')
-			;
-		else if (sign == 1 && str[i] == '-')
-			sign = -1;
-		else if ('0' <= str[i] && str[i] <= '9')
-			n = 10 * n + str[i] - '0';
-		else
-			break ;
+		sign = -1;
+		i += 1;
+	}
+	if (str[i] == '+')
+		i += 1;
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		n = 10 * n + str[i] - '0';
 		i++;
 	}
-	return (n * sign);
+	return (sign * n);
 }
