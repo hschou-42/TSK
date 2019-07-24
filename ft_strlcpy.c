ed9b43e4fc90adb03dcd285e449f40a2fd474f5c
hschou <hschou@student.42.us.org>
2019-07-23 19:58:56 -0700

    add ft_strlcpy.c
    new file

---
+++ ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hschou <hschou@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/23 19:18:39 by hschou            #+#    #+#             */
+/*   Updated: 2019/07/23 19:58:34 by hschou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	rt;
+
+	i = 0;
+	rt = 0;
+	while (src[i] != '\0')
+		rt++;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0'
+	return (rt);
+}
